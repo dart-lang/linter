4fcde8ae8210f343dd2929cbb71d931a5cb71569
pq <pquitslund@google.com>
2018-02-16 14:11:52 -0800

    error handling in case dartftmt isn’t on the PATH

--- test/validate_format_test.dart
+++ test/validate_format_test.dart
@@ -4,13 +4,18 @@ import 'package:test/test.dart';
 
 main() {
   test('validate source formatting', () async {
-    ProcessResult result = await Process
-        .run('dartfmt', ['--dry-run', '--set-exit-if-changed', '.']);
-    List<String> violations = result.stdout.toString().split('\n')
-      ..removeWhere(formattingIgnored);
-    expect(violations, isEmpty, reason: '''Some files need formatting. 
+    try {
+      ProcessResult result = await Process
+          .run('dartfmt', ['--dry-run', '--set-exit-if-changed', '.']);
+      List<String> violations = result.stdout.toString().split('\n')
+        ..removeWhere(formattingIgnored);
+      expect(violations, isEmpty, reason: '''Some files need formatting. 
   
 Run `dartfmt` and (re)commit.''');
+    } on ProcessException {
+      // This occurs, notably, on appveyor.
+      print('[WARNING] format validation skipped -- `dartfmt` not on PATH');
+    }
   });
 }
 
