67cdd0627c0164109ae88a9a300b5c31e1fa14f4
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-06-15 11:09:30 +0200

    address review comments

--- lib/src/rules/file_names.dart
+++ lib/src/rules/file_names.dart
@@ -13,11 +13,11 @@ const _details = r'''
 
 **DO** name source files using `lowercase_with_underscores`.
 
-Some file systems are not case-sensitive, so many projects require filenames
-to be all lowercase. Using a separate character allows names to still be
-readable in that form. Using underscores as the separator ensures that the name
-is still a valid Dart identifier, which may be helpful if the language later
-supports symbolic imports.
+Some file systems are not case-sensitive, so many projects require filenames to
+be all lowercase. Using a separating character allows names to still be readable
+in that form. Using underscores as the separator ensures that the name is still
+a valid Dart identifier, which may be helpful if the language later supports
+symbolic imports.
 
 **GOOD:**
 
@@ -28,6 +28,9 @@ supports symbolic imports.
 
 * `SliderMenu.dart`
 * `filesystem.dart`
+
+The lint `library_names` can be used to enforce the same kind of naming on the
+library.
 
 ''';
 
--- lib/src/rules/library_names.dart
+++ lib/src/rules/library_names.dart
@@ -13,11 +13,11 @@ const _details = r'''
 
 **DO** name libraries using `lowercase_with_underscores`.
 
-Some file systems are not case-sensitive, so many projects require filenames
-to be all lowercase.  Using a separate character allows names to still be
-readable in that form.  Using underscores as the separator ensures that the name
-is still a valid Dart identifier, which may be helpful if the language later
-supports symbolic imports.
+Some file systems are not case-sensitive, so many projects require filenames to
+be all lowercase. Using a separating character allows names to still be readable
+in that form. Using underscores as the separator ensures that the name is still
+a valid Dart identifier, which may be helpful if the language later supports
+symbolic imports.
 
 **GOOD:**
 
@@ -26,6 +26,9 @@ supports symbolic imports.
 **BAD:**
 
 * `library peg-parser;`
+
+The lint `file_names` can be used to enforce the same kind of naming on the
+file.
 
 ''';
 
