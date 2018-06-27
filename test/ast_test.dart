786252cd489670a82c19e9cf15daaf8756508345
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-06-27 16:38:06 +0200

    make travis happy

--- test/ast_test.dart
+++ test/ast_test.dart
@@ -11,16 +11,16 @@ main() {
     test('has hasConstError true with a tracked const error', () {
       final listener = new HasConstErrorListener();
       listener.onError(new AnalysisError(
-          null, null, null, HasConstErrorListener.errorCodes.first));
+          null, null, null, CompileTimeErrorCode.CONST_WITH_NON_CONST));
       expect(listener.hasConstError, isTrue);
     });
     test('has hasConstError true even if last error is not related to const',
         () {
       final listener = new HasConstErrorListener();
       listener.onError(new AnalysisError(
-          null, null, null, HasConstErrorListener.errorCodes.first));
+          null, null, null, CompileTimeErrorCode.CONST_WITH_NON_CONST));
       listener.onError(new AnalysisError(
-          null, null, null, CompileTimeErrorCode.AMBIGUOUS_EXPORT));
+          null, null, null, CompileTimeErrorCode.ACCESS_PRIVATE_ENUM_FIELD));
       expect(listener.hasConstError, isTrue);
     });
   });
