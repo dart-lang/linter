a556362324a582b8af4df6322b8d2d1747b5c282
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-10-31 17:47:33 +0100

    address review comment

--- lib/src/rules/avoid_returning_null_for_future.dart
+++ lib/src/rules/avoid_returning_null_for_future.dart
@@ -13,10 +13,8 @@ const _details = r'''
 
 **AVOID** returning null for Future.
 
-Future being used to represent a potential value, or error, that will be
-available at some time in the future it's almost always wrong to return null for
-Future. Most of the time the developer simply forgot to put an async keyword on
-the function.
+It is almost always wrong to return `null` for a `Future`.  Most of the time the
+developer simply forgot to put an `async` keyword on the function.
 
 ''';
 
