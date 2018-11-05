07dad5a28c6d1609a4d8fc4f04706a77d0de094c
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-11-05 22:51:55 +0100

    add lint unnecessary_await

---
+++ lib/src/rules/unnecessary_await.dart
@@ -0,0 +1,97 @@
+// Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:linter/src/analyzer.dart';
+
+const _desc = r'Unnecessary await keyword.';
+
+const _details = r'''
+
+Avoid using await on an expression with a type that is assignable to the return
+type.
+
+**BAD:**
+```
+Future<int> future;
+Future<int> f1() async => await future;
+Future<int> f2() async {
+  return await future;
+}
+```
+
+**GOOD:**
+```
+Future<int> future;
+Future<int> f1() => future;
+Future<int> f2() {
+  return future;
+}
+```
+
+''';
+
+class UnnecessaryAwait extends LintRule implements NodeLintRuleWithContext {
+  UnnecessaryAwait()
+      : super(
+            name: 'unnecessary_await',
+            description: _desc,
+            details: _details,
+            group: Group.style);
+
+  @override
+  void registerNodeProcessors(NodeLintRegistry registry,
+      [LinterContext context]) {
+    final visitor = new _Visitor(this);
+    registry.addExpressionFunctionBody(this, visitor);
+    registry.addReturnStatement(this, visitor);
+  }
+}
+
+class _Visitor extends SimpleAstVisitor<void> {
+  final LintRule rule;
+
+  _Visitor(this.rule);
+
+  @override
+  void visitExpressionFunctionBody(ExpressionFunctionBody node) {
+    _visit(node, node.expression.unParenthesized);
+  }
+
+  @override
+  void visitReturnStatement(ReturnStatement node) {
+    if (node.expression != null) {
+      _visit(node, node.expression.unParenthesized);
+    }
+  }
+
+  void _visit(AstNode node, Expression expression) {
+    if (expression is! AwaitExpression) return;
+
+    final type = (expression as AwaitExpression).expression.staticType;
+    if (type?.isDartAsyncFuture != true) {
+      return;
+    }
+
+    final parent = node
+        .getAncestor((e) => e is FunctionExpression || e is MethodDeclaration);
+    if (parent == null) return;
+
+    DartType returnType;
+    if (parent is FunctionExpression) {
+      returnType = parent.declaredElement?.returnType;
+    } else if (parent is MethodDeclaration) {
+      returnType = parent.declaredElement?.returnType;
+    } else {
+      throw StateError('unexpected type');
+    }
+    if (returnType != null &&
+        returnType.isDartAsyncFuture &&
+        type.isAssignableTo(returnType)) {
+      rule.reportLintForToken((expression as AwaitExpression).awaitKeyword);
+    }
+  }
+}
---
+++ test/rules/unnecessary_await.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// test w/ `pub run test -N unnecessary_await`
+
+import 'dart:async';
+
+final future = Future.value(1);
+final futureFuture = Future<Future<int>>.value(Future<int>.value(1));
+
+Future<int> f1a() async => await future; // LINT
+Future<int> f1b() async {
+  return await future; // LINT
+}
+
+Future<int> f2a() async => future; // OK
+Future<int> f2b() async {
+  return future; // OK
+}
+
+Future<int> f3a() async => await futureFuture; // OK
+Future<int> f3b() async {
+  return await futureFuture; // OK
+}
+
+class A {
+  Future<int> f1a() async => await future; // LINT
+  Future<int> f1b() async {
+    return await future; // LINT
+  }
+
+  Future<int> f2a() async => future; // OK
+  Future<int> f2b() async {
+    return future; // OK
+  }
+
+  Future<int> f3a() async => await futureFuture; // OK
+  Future<int> f3b() async {
+    return await futureFuture; // OK
+  }
+}
