33156d94222e45cc59a33ab1d4443c4344aa76e7
Phil Quitslund <pq@users.noreply.github.com>
2019-11-13 11:41:59 -0800

    Update travis.sh

--- tool/travis.sh
+++ tool/travis.sh
@@ -8,7 +8,7 @@
 set -e
 
 
-if [ "$LINTER_BOT" = "release" && "$TRAVIS_OS_NAME" = "osx" ]; then
+if [ ["$LINTER_BOT" = "release"] && ["$TRAVIS_OS_NAME" = "osx"] ]; then
   echo "Validating release..."
   dart tool/bot/version_check.dart
 
@@ -28,7 +28,7 @@ elif [ "$LINTER_BOT" = "benchmark" ]; then
     exit 1
   fi
 
-elif [ "$LINTER_BOT" = "pana_baseline" && "$TRAVIS_OS_NAME" = "osx" ]; then
+elif [ ["$LINTER_BOT" = "pana_baseline"] && ["$TRAVIS_OS_NAME" = "osx"] ]; then
   echo "Checking the linter pana baseline..."
 
   dart tool/pana_baseline.dart
