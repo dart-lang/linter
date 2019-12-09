cc020a7902105eb74e3e77acbc4f603518f8d4b2
pq <pquitslund@gmail.com>
2019-12-09 10:31:45 -0800

    relative import

--- lib/src/rules/no_logic_in_create_state.dart
+++ lib/src/rules/no_logic_in_create_state.dart
@@ -4,9 +4,9 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:linter/src/util/flutter_utils.dart';
 
 import '../analyzer.dart';
+import '../util/flutter_utils.dart';
 
 const _desc = r"Don't put any logic in createState.";
 
