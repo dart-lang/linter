8ad685fb4b6cfcf0f5fff485f1184c3a0da96e12
pq <pquitslund@gmail.com>
2020-07-06 07:51:27 -0700

    => matcher

--- lib/src/test_utilities/annotation_matcher.dart
+++ lib/src/test_utilities/annotation_matcher.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/error/error.dart';
-import 'package:test/test.dart';
+import 'package:matcher/matcher.dart';
 
 import 'annotation.dart';
 
