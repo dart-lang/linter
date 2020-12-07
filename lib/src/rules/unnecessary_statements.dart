f88f95626a8f258d387d5a3746de57acd0acdd54
pq <pquitslund@gmail.com>
2020-12-07 13:40:18 -0800

    drop unnecessary `new`

--- lib/src/rules/unnecessary_statements.dart
+++ lib/src/rules/unnecessary_statements.dart
@@ -37,7 +37,7 @@ Usually code like this indicates an incomplete thought, and is a bug.
 **GOOD:**
 ```
 some.method();
-new SomeClass();
+SomeClass();
 methodOne();
 methodTwo();
 foo ? bar() : baz();
