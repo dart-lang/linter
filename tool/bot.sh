00896a7f8fbf1d7d50c4997605ffdcbaf16116c8
Phil Quitslund <pq@users.noreply.github.com>
2021-02-12 14:45:47 -0800

    more unsound

--- tool/bot.sh
+++ tool/bot.sh
@@ -9,8 +9,8 @@ set -e
 
 if [ "$LINTER_BOT" = "release" ]; then
   echo "Validating release..."
-  dart tool/bot/version_check.dart
-  dart tool/bot/rule_doc_check.dart
+  dart --no-sound-null-safety tool/bot/version_check.dart
+  dart --no-sound-null-safety tool/bot/rule_doc_check.dart
 
 # https://github.com/dart-lang/linter/issues/2014
 #elif [ "$LINTER_BOT" = "score" ]; then
