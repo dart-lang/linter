a7a2abb2d447e57674e7912c7ed81503bff78c3d
Sam Rawlins <srawlins@google.com>
2021-06-10 10:43:48 -0700

    Allow a single trailing underscore in lowerCamelCase

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -190,10 +190,11 @@ void main() {
       'bar\$',
       'foo\$Generated',
       'foo\$Generated\$Bar'
+      'foo_',
     ];
     testEach(good, isLowerCamelCase, isTrue);
 
-    var bad = ['Foo', 'foo_', 'foo_bar', '_X'];
+    var bad = ['Foo', 'foo_bar', '_X'];
     testEach(bad, isLowerCamelCase, isFalse);
   });
 
