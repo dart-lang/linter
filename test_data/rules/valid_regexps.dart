5b06059ca24da057b4309121ea47ef9e743f8827
pq <pquitslund@gmail.com>
2021-07-01 11:04:40 -0700

    less comments

--- test_data/rules/valid_regexps.dart
+++ test_data/rules/valid_regexps.dart
@@ -8,7 +8,7 @@ var bad = RegExp('('); //LINT
 var good = RegExp('[(]'); //OK
 var interpolated = '';
 var skipped = RegExp('( $interpolated'); //OK -- skipped
-// /// https://stackoverflow.com/questions/61151471/regexp-for-unicode-13-emojis
+/// https://stackoverflow.com/questions/61151471/regexp-for-unicode-13-emojis
 var emojis = RegExp(
     r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])',
     unicode: true); //OK
