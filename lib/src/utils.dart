072da8c1350a603fb8b1c2601e30663b5b29a2a2
pq <pquitslund@gmail.com>
2021-07-22 13:01:47 -0700

    - unused import

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'ast.dart';
-
 final _identifier = RegExp(r'^([(_|$)a-zA-Z]+([_a-zA-Z0-9])*)$');
 
 final _lowerCamelCase = RegExp(
