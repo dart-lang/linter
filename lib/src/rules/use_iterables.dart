3819d63a5845acb97aa24a48003cc593243d91c1
pq <pquitslund@gmail.com>
2021-09-14 13:25:20 -0700

    moar cases

--- lib/src/rules/use_iterables.dart
+++ lib/src/rules/use_iterables.dart
@@ -4,17 +4,17 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:analyzer/dart/element/type.dart';
-import 'package:analyzer/dart/element/type_provider.dart';
 
 import '../analyzer.dart';
+import '../util/dart_type_utilities.dart';
 
-const _desc = r'Use lazy iterables.';
+const _desc = r"Don't ignore the value of lazy iterables.";
 
 const _details = r'''
-**DO** use lazy iterables created by `iterable.map`. Since they are lazily
-instantiated, the `map` function will not be evaluated until use which can be
-confusing.  Alternatively, consider preferring `forEach`.
+**DON'T** ignore the value of lazy iterables created by `iterable.map`. Since 
+these iterables are lazily instantiated, the `map` function is not evaluated
+until use which can be surprising.  Alternatively, consider preferring 
+`forEach`.
 
 **BAD:**
 ```dart
@@ -42,49 +42,43 @@ class UseIterables extends LintRule {
   @override
   void registerNodeProcessors(
       NodeLintRegistry registry, LinterContext context) {
-    var visitor = _Visitor(this, context);
+    var visitor = _Visitor(this);
     registry.addMethodInvocation(this, visitor);
   }
 }
 
 class _Visitor extends SimpleAstVisitor {
   final LintRule rule;
-  final TypeProvider typeProvider;
-
-  _Visitor(this.rule, LinterContext context)
-      : typeProvider = context.typeProvider;
+  _Visitor(this.rule);
 
   @override
   visitMethodInvocation(MethodInvocation node) {
-    if (!_isIterable(node.staticType)) {
-      return;
-    }
-    if (node.methodName.name != 'map') {
-      return;
-    }
-    if (_isUsed(node)) {
-      return;
-    }
+    if (node.methodName.name != 'map') return;
+    if (!_implementsIterable(node.realTarget)) return;
+    if (_isUsed(node)) return;
 
     rule.reportLint(node.methodName);
   }
 
-  bool _isIterable(DartType? type) =>
-      type != null && type.asInstanceOf(typeProvider.iterableElement) != null;
+  static bool _implementsIterable(Expression? target) =>
+      target != null &&
+      !DartTypeUtilities.implementsInterface(
+          target.staticType, 'Iterable', 'dart.core');
 
   static bool _isUsed(AstNode node) {
     var parent = node.parent;
-    if (parent == null) {
-      return false;
-    }
+    if (parent == null) return false;
 
     if (parent is ParenthesizedExpression ||
+        parent is AsExpression ||
+        parent is BinaryExpression ||
         parent is ConditionalExpression ||
         parent is CascadeExpression) {
       return _isUsed(parent);
     }
 
     return parent is ArgumentList ||
+        parent is IndexExpression ||
         parent is VariableDeclaration ||
         parent is MethodInvocation ||
         parent is PropertyAccess ||
--- test_data/rules/use_iterables.dart
+++ test_data/rules/use_iterables.dart
@@ -7,14 +7,24 @@
 import 'dart:core';
 import 'dart:core' as core;
 
+extension I on Iterable {
+  operator+(Iterable other) => [];
+}
+
+class A {
+  Iterable map(Function(Object) f) => [];
+}
+
 void f() {
+  A().map(print); // OK
   [].map(print); // LINT
   var s = { 1 };
   s.map(print); // LINT
   [].map(print).toList(); // OK
-  [].map(print).last; // OK
+  [].map(print).first; // OK
   print([].map(print)); // OK
   var iter = [].map(print); // OK
+  var iter2 = [1].map(print) as Iterable; // OK
   var s1 = {
     [].map(print), // OK
   };
@@ -29,8 +39,28 @@ void f() {
 
   core.List.empty().map(print); // LINT
   print(core.List.empty().map(print)); // OK
-  var b = [].map(print) == null; // LINT
-  print([].map(print) == null); // LINT
+  var b = [].map(print) == null; // OK
+  print([].map(print) == null); // OK
+  var x = [1].map(print) + []; // OK
+  var i = [1].map(print)[0]; // OK
+  [1]..map(print); // LINT
+  [1]..map(print).toList(); // OK
+  [1]..map(print)..toList(); // LINT
+
+  for ([1].map(print); ; ) { // LINT
+    print('?');
+  }
+  for (; ; [1].map(print)) { // LINT
+    print('?');
+  }
+  ([1].map(print)); // LINT
+  [1].map(print) as Iterable; // LINT
+  if ([1].map(print) is Iterable) print('!'); // LINT
+  iterOrNull()?.map(print); // LINT
+  iterOrNull()?.map(print).first; // OK
+  true ? [].map(print) : []; // LINT
+  var iter3 = true ? [].map(print) : []; // OK
 }
 
 Iterable iter() => [].map(print); // OK
+Iterable? iterOrNull() => [].map(print); // OK
