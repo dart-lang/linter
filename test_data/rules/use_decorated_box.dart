04df1aa960c813221e31544cb7e4435939892de4
minhqdao <asiancorporator@yahoo.de>
2021-11-13 17:22:19 +0100

    Add UseDecoratedBox

---
+++ test_data/rules/use_decorated_box.dart
@@ -0,0 +1,60 @@
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// test w/ `dart test -N use_decorated_box`
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/painting.dart';
+
+Widget containerWithoutArguments() {
+  return Container(); // OK
+}
+
+Widget containerWithKey() {
+  return Container( // OK
+    key: Key('abc'),
+  );
+}
+
+Widget containerWithDecoration() {
+  return Container( // LINT
+    decoration: BoxDecoration(),
+  );
+}
+
+Widget containerWithChild() {
+  return Container( // OK
+    child: SizedBox(),
+  );
+}
+
+Widget containerWithKeyAndChild() {
+  return Container( // OK
+    key: Key('abc'),
+    child: SizedBox(),
+  );
+}
+
+Widget containerWithKeyAndDecoration() {
+  return Container( // LINT
+    key: Key('abc'),
+    decoration: BoxDecoration(),
+  );
+}
+
+Widget containerWithDecorationAndChild() {
+  return Container( // LINT
+    decoration: BoxDecoration(),
+    child: SizedBox(),
+  );
+}
+
+Widget containerWithKeyAndDecorationAndChild() {
+  return Container( // LINT
+    key: Key('abc'),
+    decoration: BoxDecoration(),
+    child: SizedBox(),
+  );
+}
