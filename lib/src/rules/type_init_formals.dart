18b9ee377b66df8c1d1914b0f589edc33e334ca5
pq <pquitslund@gmail.com>
2022-01-24 13:08:18 -0800

    add super initializer example

--- lib/src/rules/type_init_formals.dart
+++ lib/src/rules/type_init_formals.dart
@@ -16,8 +16,8 @@ From the [style guide](https://dart.dev/guides/language/effective-dart/style/):
 
 **DON'T** type annotate initializing formals.
 
-If a constructor parameter is using `this.x` to initialize a field, then the
-type of the parameter is understood to be the same type as the field.
+If a constructor parameter is using `this.x` or `super.x` to initialize a field,
+then the type of the parameter is understood to be the same type as the field.
 
 **GOOD:**
 ```dart
@@ -35,6 +35,29 @@ class Point {
 }
 ```
 
+**GOOD:**
+```dart
+class A {
+  int a;
+  A(this.a);
+}
+
+class B extends A {
+  B(super.a);
+}
+```
+
+**BAD:**
+```dart
+class A {
+  int a;
+  A(this.a);
+}
+
+class B extends A {
+  B(int super.a);
+}
+```
 ''';
 
 class TypeInitFormals extends LintRule {
