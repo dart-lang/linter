5825732b035523ac0f800e50d2087923db797067
pq <pquitslund@gmail.com>
2022-03-16 10:07:16 -0700

    + super formals

--- lib/src/rules/use_super_parameters.dart
+++ lib/src/rules/use_super_parameters.dart
@@ -173,6 +173,7 @@ class _Visitor extends SimpleAstVisitor {
       for (var i = 0; i < constructorParams.length && !match; ++i) {
         var constructorParam = constructorParams[i];
         if (constructorParam is FieldFormalParameter) continue;
+        if (constructorParam is SuperFormalParameter) continue;
         var constructorElement = constructorParam.declaredElement;
         if (constructorElement == null) continue;
         if (referencedParameters.contains(constructorElement)) continue;
