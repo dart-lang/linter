7e911fc818ebe8252373fb69cc6bcb7da1da9612
Erik Ernst <eernst@google.com>
2022-07-13 15:26:34 +0200

    WIP

--- lib/src/rules/avoid_unstable_final_fields.dart
+++ lib/src/rules/avoid_unstable_final_fields.dart
@@ -6,7 +6,6 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/type.dart';
 
 import '../analyzer.dart';
 
