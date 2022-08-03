ba46aa7607b3525582409c772b0e1c73c90a08ce
Brett Morgan <brett.morgan@gmail.com>
2022-08-03 13:53:17 +1000

    Add a the to make for betterer english

--- lib/src/rules/avoid_function_literals_in_foreach_calls.dart
+++ lib/src/rules/avoid_function_literals_in_foreach_calls.dart
@@ -17,7 +17,7 @@ const _details = r'''
 **AVOID** using `forEach` with a function literal.
 
 The `for` loop enables a developer to be clear and explicit as to their intent.
-A return in the body of the `for` loop returns from the body of function, 
+A return in the body of the `for` loop returns from the body of the function, 
 where as a return in the body of the `forEach` closure only returns a value 
 for that iteration of the `forEach`. The body of a `for` loop can contain 
 `await`s, while the closure body of a `forEach` cannot.
