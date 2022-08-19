cdc6e7f4d6536e664516096273f72072d093740b
Nate Bosch <nbosch@google.com>
2022-08-19 23:19:29 +0000

    Expand test cases

--- test_data/rules/avoid_implicit_call_tearoffs.dart
+++ test_data/rules/avoid_implicit_call_tearoffs.dart
@@ -4,20 +4,64 @@
 
 // test w/ `dart test -N avoid_implicit_call_tearoffs`
 
-class Callable {
+class C {
   void call() {}
+  void other() {}
 }
 
 void callIt(void Function() f) {
   f();
 }
 
+void Function() r1() => C(); // LINT
+void Function() r2() => C().call; // OK
+void Function() r3(C c) => c; // LINT
+void Function() r4(C c) => c.call; // OK
+
+void Function() r5(C? c1, C c2) {
+  return c1 ?? c2; // LINT
+}
+
+void Function() r6(C? c1, C c2) {
+  return c1?.call ?? c2.call; // OK
+}
+
+void Function() r7() {
+  return C()..other(); // LINT
+}
+
+void Function() r8() {
+  return (C()..other()).call; // OK
+}
+
+List<void Function()> r9(C c) {
+  return [c]; // LINT
+}
+
+List<void Function()> r10(C c) {
+  return [c.call]; // OK
+}
+
 void main() {
-  final c = Callable();
-  callIt(c); //LINT
-  c as Function; //TODO
-  Function f1 = c; //LINT
-  callIt(Callable()); //LINT
-  Callable() as Function; //TODO
-  Function f2 = Callable(); //LINT
+  callIt(C()); // LINT
+  callIt(C().call); // OK
+  Function f1 = C(); // LINT
+  Function f2 = C().call; // OK
+  void Function() f3 = C(); // LINT
+  void Function() f4 = C().call; // OK
+
+  final c = C();
+  callIt(c); // LINT
+  callIt(c.call); // OK
+  Function f5 = c; // LINT
+  Function f6 = c.call; // OK
+  void Function() f7 = c; // LINT
+  void Function() f8 = c.call; // OK
+
+  <void Function()>[
+    C(), // LINT
+    C().call, //OK
+    c, // LINT
+    c.call, // OK
+  ];
 }
