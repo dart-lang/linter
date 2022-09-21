e8dd9000993a0ed5dd983dacb873cab1032cd6a7
Nate Bosch <nbosch@google.com>
2022-09-21 15:40:43 +0000

    Revert "Add missed case for prefer_collection_literals"
    This reverts commit 6e295f8041fb6c682dd83e2870e4bd6654f78ec9.

--- test_data/rules/prefer_collection_literals.dart
+++ test_data/rules/prefer_collection_literals.dart
@@ -64,7 +64,6 @@ void main() {
   var sss = Set.from(iter); // OK
 
   LinkedHashSet<String> sss1 = <int, LinkedHashSet<String>>{}.putIfAbsent(3, () => LinkedHashSet<String>()); // OK
-  var sss2 = <int, Set<String>>{}.putIfAbsent(0, () => LinkedHashSet()); // LINT
 
   var lhs = LinkedHashSet(equals: (a, b) => false, hashCode: (o) => 13)..addAll({}); // OK
 
