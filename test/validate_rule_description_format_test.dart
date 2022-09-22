4ea3024303f598071da2e9488ad1d47d48e0e7a0
pq <pquitslund@gmail.com>
2022-09-22 09:48:21 -0700

    ++

--- test/validate_rule_description_format_test.dart
+++ test/validate_rule_description_format_test.dart
@@ -2,11 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
-import 'package:linter/src/rules.dart';
-import 'package:test/test.dart';
 import 'package:analyzer/src/lint/registry.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('rule doc format', () {
