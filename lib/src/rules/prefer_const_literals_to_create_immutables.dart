2d5a50b597c3045be26d412a6b9bf36890ff0ff0
pq <pquitslund@gmail.com>
2022-10-24 12:08:34 -0700

    --

--- lib/src/rules/prefer_const_literals_to_create_immutables.dart
+++ lib/src/rules/prefer_const_literals_to_create_immutables.dart
@@ -121,8 +121,6 @@ class _Visitor extends SimpleAstVisitor<void> {
     }
 
     InterfaceType? current = type;
-    // TODO(a14n) the is check looks unnecessary but prevents https://github.com/dart-lang/sdk/issues/33210
-    // for now it's not clear how this can happen
     while (current != null) {
       for (var annotation in current.element2.metadata) {
         if (_isImmutable(annotation.element)) return true;
