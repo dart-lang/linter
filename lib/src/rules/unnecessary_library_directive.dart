fe6356af8b8dd15036d47e918ae1ec1f70eea9dd
Kevin Moore <kevmoo@google.com>
2022-10-26 17:12:58 -0700

    phil nits

--- lib/src/rules/unnecessary_library_directive.dart
+++ lib/src/rules/unnecessary_library_directive.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, the Dart project authors. Please see the AUTHORS file
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -10,7 +10,7 @@ import '../analyzer.dart';
 const _name = 'unnecessary_library_directive';
 
 const _desc =
-    'Avoid library directives unless that have documentation comments or '
+    'Avoid library directives unless they have documentation comments or '
     'annotations.';
 
 const _details = r'''
