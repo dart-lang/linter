5c5db6392bb24a6b20b1a63601d19c52ced2969b
pq <pquitslund@gmail.com>
2022-12-20 14:17:20 -0800

    ensure `one_member_abstracts` doesn't lint sealed classes

--- test/rules/all.dart
+++ test/rules/all.dart
@@ -53,6 +53,7 @@ import 'non_constant_identifier_names_test.dart'
     as non_constant_identifier_names;
 import 'null_closures_test.dart' as null_closures;
 import 'omit_local_variable_types_test.dart' as omit_local_variable_types;
+import 'one_member_abstracts_test.dart' as one_member_abstracts;
 import 'overridden_fields_test.dart' as overridden_fields;
 import 'parameter_assignments_test.dart' as parameter_assignments;
 import 'prefer_asserts_in_initializer_lists_test.dart'
@@ -132,6 +133,7 @@ void main() {
   non_constant_identifier_names.main();
   null_closures.main();
   omit_local_variable_types.main();
+  one_member_abstracts.main();
   overridden_fields.main();
   parameter_assignments.main();
   prefer_asserts_in_initializer_lists.main();
---
+++ test/rules/one_member_abstracts_test.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import '../rule_test_support.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(OneMemberAbstractsTest);
+  });
+}
+
+@reflectiveTest
+class OneMemberAbstractsTest extends LintRuleTest {
+  @override
+  String get lintRule => 'one_member_abstracts';
+
+  @override
+  List<String> get experiments => ['sealed-class'];
+
+  test_sealed_noDiagnostic() async {
+    await assertNoDiagnostics(r'''
+sealed class C {
+  void f() { }
+}
+''');
+  }
+}
+
