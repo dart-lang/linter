65437113aebc8b1035c1daead646f65b41ae32d0
Ahmed Ashour <asashour@yahoo.com>
2023-01-05 23:09:01 +0100

    Format

--- lib/src/rules/always_specify_types.dart
+++ lib/src/rules/always_specify_types.dart
@@ -106,8 +106,7 @@ class _Visitor extends SimpleAstVisitor<void> {
 
   static const LintCode specifyTypeCode = LintCode('always_specify_types',
       "Missing type annotation.", // ignore: prefer_single_quotes
-      correctionMessage:
-          "Try specifying the type '{0}'.");
+      correctionMessage: "Try specifying the type '{0}'.");
 
   final LintRule rule;
 
