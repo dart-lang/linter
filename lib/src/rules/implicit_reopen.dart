89172fc8d0b6a62349256cfc5f2aab4b56bed0ed
pq <pquitslund@gmail.com>
2023-03-01 12:59:22 -0800

    + implicit_reopen

---
+++ lib/src/rules/implicit_reopen.dart
@@ -0,0 +1,229 @@
+// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
+
+import '../analyzer.dart';
+
+const _desc = r"Don't implicitly reopen classes or mixins";
+
+/// todo(pq): link out to the spec or dart.dev docs.
+const _details = r'''
+Using the `interface`, `final`, `base`, `mixin`, and `sealed` class modifiers,
+authors can control whether classes and mixins allow being implemented,
+extended, and/or mixed in from outside of the library where they're defined.
+In some cases, it's possible for an author to inadvertantly relax these controls
+and implicitly "reopen" a class or mixin. This lint guards against that,
+requiring such cases to be made explicit with the 
+[`@reopen`](https://pub.dev/documentation/meta/latest/meta/reopen-constant.html)
+annotation in `package:meta`.
+
+**BAD:**
+```dart
+interface class I {}
+
+class C extends I {}
+```
+
+**GOOD:**
+```dart
+interface class I {}
+
+final class C extends I {}
+```
+
+```dart
+import 'package:meta/meta.dart';
+
+interface class I {}
+
+@reopen
+class C extends I {}
+```
+''';
+
+class ImplicitReopen extends LintRule {
+  static const LintCode code = LintCode('implicit_reopen',
+      "The class '{0}' reopens '{1}' because it is not marked '{2}'",
+      correctionMessage:
+          "Try marking {0} '{2}' or annotating it with '@reopen'");
+
+  ImplicitReopen()
+      : super(
+            name: 'implicit_reopen',
+            description: _desc,
+            details: _details,
+            state: State.experimental(),
+            group: Group.errors);
+
+  @override
+  LintCode get lintCode => code;
+
+  @override
+  void registerNodeProcessors(
+      NodeLintRegistry registry, LinterContext context) {
+    var visitor = _Visitor(this);
+    registry.addClassDeclaration(this, visitor);
+  }
+}
+
+class _Visitor extends SimpleAstVisitor {
+  final LintRule rule;
+
+  _Visitor(this.rule);
+
+  void reportLint(
+    NamedCompilationUnitMember member, {
+    required InterfaceElement target,
+    required InterfaceElement other,
+    required String reason,
+  }) {
+    rule.reportLintForToken(member.name,
+        arguments: [target.name, other.name, reason]);
+  }
+
+  @override
+  void visitClassDeclaration(ClassDeclaration node) {
+    var classElement = node.declaredElement;
+    if (classElement == null) return;
+    if (classElement.hasReopen) return;
+
+    var library = classElement.library;
+
+    // | subtype          |  supertype   | `extends`/`with` |
+    // | _none_ or `base` |  `interface` | lint             |
+    // | `base`           |  `final`     | lint             |
+    if (classElement.hasNoModifiers || classElement.isBase) {
+      var supertype = classElement.superElement;
+      if (supertype.library == library) {
+        if (supertype.isInterface || supertype.isInducedInterface) {
+          reportLint(node,
+              target: classElement, other: supertype!, reason: 'interface');
+          return;
+        }
+      }
+
+      for (var m in classElement.mixins) {
+        var mixin = m.element;
+        if (mixin.library != library) continue;
+        if (mixin.isInterface || mixin.isInducedInterface) {
+          reportLint(node,
+              target: classElement, other: mixin, reason: 'interface');
+          return;
+        }
+      }
+    }
+
+    if (classElement.isBase) {
+      var supertype = classElement.superElement;
+      if (supertype.library == library) {
+        if (supertype.isFinal || supertype.isInducedFinal) {
+          reportLint(node,
+              target: classElement, other: supertype!, reason: 'final');
+          return;
+        }
+      }
+
+      for (var m in classElement.mixins) {
+        var mixin = m.element;
+        if (mixin.library != library) continue;
+        if (mixin.isFinal || mixin.isInducedFinal) {
+          reportLint(node, target: classElement, other: mixin, reason: 'final');
+          return;
+        }
+      }
+    }
+  }
+}
+
+extension on InterfaceElement? {
+  List<InterfaceType> get interfaces => this?.interfaces ?? <InterfaceType>[];
+
+  bool get isFinal {
+    var self = this;
+    return self != null && self.isFinal;
+  }
+
+  /// A sealed declaration D is considered final if it has a direct extends or
+  /// with superinterface which is final, or it has a direct superinterface
+  /// which is base as well as a direct extends or with superinterface which is
+  /// interface.
+  bool get isInducedFinal {
+    if (!isSealed) return false;
+
+    if (superElement.isFinal) return true;
+    if (mixins.any((m) => m.element.isFinal)) return true;
+
+    for (var i in interfaces) {
+      if (i.element.isBase) {
+        if (superElement.isInterface) return true;
+        if (mixins.any((m) => m.element.isInterface)) return true;
+      }
+    }
+
+    return false;
+  }
+
+  /// A sealed declaration D is considered interface if it has a direct extends
+  /// or with superinterface which is interface.
+  bool get isInducedInterface {
+    if (!isSealed) return false;
+
+    if (superElement.isInterface) return true;
+    if (mixins.any((m) => m.element.isInterface)) return true;
+
+    return false;
+  }
+
+  bool get isInterface {
+    var self = this;
+    return self != null && self.isInterface;
+  }
+
+  bool get isSealed {
+    var self = this;
+    return self != null && self.isSealed;
+  }
+
+  LibraryElement? get library => this?.library;
+
+  List<InterfaceType> get mixins => this?.mixins ?? <InterfaceType>[];
+
+  InterfaceElement? get superElement => this?.supertype?.element;
+}
+
+extension on InterfaceElement {
+  bool get hasNoModifiers => !isInterface && !isBase && !isSealed && !isFinal;
+
+  bool get isBase {
+    var self = this;
+    if (self is ClassElement) return self.isBase;
+    if (self is MixinElement) return self.isBase;
+    return false;
+  }
+
+  bool get isFinal {
+    var self = this;
+    if (self is ClassElement) return self.isFinal;
+    if (self is MixinElement) return self.isFinal;
+    return false;
+  }
+
+  bool get isInterface {
+    var self = this;
+    if (self is ClassElement) return self.isInterface;
+    if (self is MixinElement) return self.isInterface;
+    return false;
+  }
+
+  bool get isSealed {
+    var self = this;
+    if (self is ClassElement) return self.isSealed;
+    if (self is MixinElement) return self.isSealed;
+    return false;
+  }
+}
---
+++ test/rules/implicit_reopen_test.dart
@@ -0,0 +1,195 @@
+// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import '../rule_test_support.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(ImplicitReopenTest);
+  });
+}
+
+@reflectiveTest
+class ImplicitReopenTest extends LintRuleTest {
+  @override
+  bool get addMetaPackageDep => true;
+
+  @override
+  List<String> get experiments => ['class-modifiers', 'sealed-class'];
+
+  @override
+  String get lintRule => 'implicit_reopen';
+
+  test_extends_class_classFinal_ok() async {
+    await assertNoDiagnostics(r'''
+final class F {}
+
+class C extends F {}
+''');
+  }
+
+  test_extends_class_classInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+class C extends I {}
+''', [
+      lint(28, 1),
+    ]);
+  }
+
+  test_extends_class_classInterface_outsideLib_ok() async {
+    newFile('$testPackageLibPath/a.dart', r'''
+interface class I {}
+''');
+
+    await assertDiagnostics(r'''
+import 'a.dart';
+
+class C extends I {}
+''', [
+      error(CompileTimeErrorCode.INTERFACE_CLASS_EXTENDED_OUTSIDE_OF_LIBRARY,
+          34, 1),
+    ]);
+  }
+
+  test_extends_class_classInterface_reopened_ok() async {
+    await assertNoDiagnostics(r'''
+import 'package:meta/meta.dart';
+
+interface class I {}
+
+@reopen
+class C extends I {}
+''');
+  }
+
+  test_extends_class_classSealed_classInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+sealed class S extends I {}
+
+class C extends S {}
+''', [
+      lint(57, 1),
+    ]);
+  }
+
+  test_extends_class_classSealed_mixinInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+sealed class S extends I {}
+
+class C extends S {}
+''', [
+      lint(57, 1),
+    ]);
+  }
+
+  test_extends_classBase_classFinal() async {
+    await assertDiagnostics(r'''
+final class F {}
+
+base class B extends F {}
+''', [
+      lint(29, 1),
+    ]);
+  }
+
+  test_extends_classBase_classInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+base class B extends I {}
+''', [
+      lint(33, 1),
+    ]);
+  }
+
+  test_extends_classBase_classSealed_classFinal() async {
+    await assertDiagnostics(r'''
+final class F {}
+
+sealed class S extends F {}
+
+base class C extends S {}
+''', [
+      lint(58, 1),
+    ]);
+  }
+
+  test_extends_classBase_classSealed_classInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+sealed class S extends I {}
+
+base class C extends S {}
+''', [
+      lint(62, 1),
+    ]);
+  }
+
+  test_extends_classBase_classSealed_mixinInterface() async {
+    await assertDiagnostics(r'''
+interface class I {}
+
+sealed class S extends I {}
+
+base class C extends S {}
+''', [
+      lint(62, 1),
+    ]);
+  }
+
+  test_extends_classFinal_classInterface_ok() async {
+    await assertNoDiagnostics(r'''
+interface class I {}
+
+final class C extends I {}
+''');
+  }
+
+  test_with_class_mixinFinal_ok() async {
+    await assertNoDiagnostics(r'''
+final mixin M {}
+
+class C with M {}
+''');
+  }
+
+  test_with_class_mixinInterface() async {
+    await assertDiagnostics(r'''
+interface mixin M {}
+
+class C with M {}
+''', [
+      lint(28, 1),
+    ]);
+  }
+
+  test_with_classBase_mixinFinal() async {
+    await assertDiagnostics(r'''
+final mixin M {}
+
+base class B with M {}
+''', [
+      lint(29, 1),
+    ]);
+  }
+
+  test_with_classBase_mixinInterface() async {
+    await assertDiagnostics(r'''
+interface mixin M {}
+
+base class B with M {}
+''', [
+      lint(33, 1),
+    ]);
+  }
+}
