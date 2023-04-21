71d8a93fd9e9ab1d04028e5f9ae80e31aedb7a9a
pq <pquitslund@gmail.com>
2023-04-21 13:53:48 -0700

    --

--- tool/gh/get_labels.dart
+++ tool/gh/get_labels.dart
@@ -2,11 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert';
-
 import 'package:args/args.dart';
 import 'package:github/github.dart';
-import 'package:http/http.dart' as http;
 
 import '../github.dart';
 
