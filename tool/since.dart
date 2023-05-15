a7ae5cc085ec8600ceb5524ecf1fe27b908c43f3
Parker Lougheed <parlough@gmail.com>
2023-05-15 16:46:32 -0500

    Add argument parsing

--- tool/since.dart
+++ tool/since.dart
@@ -5,6 +5,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:collection/collection.dart';
 import 'package:github/github.dart';
 import 'package:pub_semver/pub_semver.dart';
@@ -12,21 +13,66 @@ import 'package:yaml/yaml.dart';
 
 import 'crawl.dart';
 
-void main() async {
-  var sinceInfo = await getSinceMap();
+void main(List<String> args) async {
+  var parser = ArgParser()
+    ..addOption(
+      'token',
+      abbr: 't',
+      help: 'Specifies a GitHub auth token.',
+    )
+    ..addFlag(
+      'linter',
+      abbr: 'l',
+      help: 'Prints out latest linter rule to linter release information.',
+    )
+    ..addFlag(
+      'sdk',
+      abbr: 's',
+      help: 'Prints out latest SDK release to linter release information.',
+    );
 
-  for (var MapEntry(key: lintName, value: sinceInfo) in sinceInfo.entries) {
-    var sinceLinter = sinceInfo.sinceLinter;
-    if (sinceLinter != null) {
-      print('$lintName: $sinceLinter');
+  ArgResults options;
+  try {
+    options = parser.parse(args);
+  } on FormatException {
+    print(parser.usage);
+    return;
+  }
+
+  var printLinter = options['linter'] == true;
+  var printSdk = options['sdk'] == true;
+
+  if (!printLinter && !printSdk) {
+    print('Either --linter or --sdk must be specified!');
+    return;
+  }
+
+  var token = options['token'];
+  var auth = token is String ? Authentication.withToken(token) : null;
+
+  var sinceInfo = await getSinceMap(auth);
+
+  if (printLinter) {
+    for (var MapEntry(key: lintName, value: sinceInfo) in sinceInfo.entries) {
+      var sinceLinter = sinceInfo.sinceLinter;
+      if (sinceLinter != null) {
+        print('$lintName: $sinceLinter');
+      }
+    }
+
+    if (printSdk) {
+      print('\n================\n');
     }
   }
 
-  var sinceSdk = await getDartSdkMap();
+  if (printSdk) {
+    var sinceSdk = await getDartSdkMap(auth);
 
-  for (var MapEntry(key: sdkVersion, value: linterVersion) in sinceSdk.entries
-      .sorted((a, b) => Version.parse(b.key).compareTo(Version.parse(a.key)))) {
-    print('$sdkVersion: $linterVersion');
+    for (var MapEntry(key: sdkVersion, value: linterVersion) in sinceSdk.entries
+        .sorted(
+            (a, b) => Version.parse(b.key).compareTo(Version.parse(a.key)))) {
+      print('$sdkVersion: $linterVersion');
+    }
   }
 }
 
