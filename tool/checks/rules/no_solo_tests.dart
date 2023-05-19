fa47910613fe9f93b17867ff1207b255d4236731
pq <pquitslund@gmail.com>
2023-05-19 15:20:12 -0700

    improve check

--- tool/checks/rules/no_solo_tests.dart
+++ tool/checks/rules/no_solo_tests.dart
@@ -4,6 +4,7 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/element.dart';
 import 'package:linter/src/analyzer.dart';
 
 const _desc = r"Don't commit soloed tests.";
@@ -38,11 +39,12 @@ class NoSoloTests extends LintRule {
   @override
   void registerNodeProcessors(
       NodeLintRegistry registry, LinterContext context) {
-    var visitor = _Visitor(this);
-    registry.addMethodDeclaration(this, visitor);
+    if (context.inTestDir(context.currentUnit.unit)) {
+      var visitor = _Visitor(this);
+      registry.addMethodDeclaration(this, visitor);
+    }
   }
 }
-
 
 class _Visitor extends SimpleAstVisitor<void> {
   final LintRule rule;
@@ -57,9 +59,18 @@ class _Visitor extends SimpleAstVisitor<void> {
     }
 
     for (var annotation in node.metadata) {
-      if (annotation.name.name == 'soloTest') {
+      if (annotation.isSoloTest) {
         rule.reportLint(annotation);
       }
     }
+  }
+}
+
+extension on Annotation {
+  bool get isSoloTest {
+    var element = this.element;
+    return element is PropertyAccessorElement &&
+        element.name == 'soloTest' &&
+        element.library.name == 'test_reflective_loader';
   }
 }
