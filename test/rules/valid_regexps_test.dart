7dc1b13e15904a8b9bfaeb3e36ca88654d7e92cf
Sam Rawlins <srawlins@google.com>
2023-07-16 07:44:45 -0700

    Update test/rules/valid_regexps_test.dart
    Co-authored-by: Albert Mañosa <albertmasa2@gmail.com>

--- test/rules/valid_regexps_test.dart
+++ test/rules/valid_regexps_test.dart
@@ -22,7 +22,7 @@ class ValidRegexpsTest extends LintRuleTest {
     await assertNoDiagnostics(r'''
 var e = RegExp(
     r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])',
-    unicode: true); //OK
+    unicode: true);
 ''');
   }
 
