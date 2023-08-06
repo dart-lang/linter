0b416ce38dbc4fb3bc010589377891feba7d0742
Sam Rawlins <srawlins@google.com>
2023-08-06 07:30:30 -1000

    Fix null-aware-operators

--- test/rules/prefer_null_aware_operators_test.dart
+++ test/rules/prefer_null_aware_operators_test.dart
@@ -17,146 +17,79 @@ class PreferNullAwareOperatorsTest extends LintRuleTest {
   @override
   String get lintRule => 'prefer_null_aware_operators';
 
-  test_nullableEqualEqualNull_null_elsePropertyAccess() async {
-    await assertDiagnostics(r'''
+  test_identifierEqualEqualNull_null_elseBinaryExpression() async {
+    await assertNoDiagnostics(r'''
 void f(A? a) {
-  a == null ? null : a.b;
+  a == null ? null : a.b + 10;
 }
 
-class A {
+abstract class A {
   int get b;
 }
-''', [
-      // TODO
-    ]);
+''');
   }
 
-  test_nullableEqualEqualNull_null_elseMethodCall() async {
+  test_identifierEqualEqualNull_null_elseMethodCall() async {
     await assertDiagnostics(r'''
 void f(A? a) {
   a == null ? null : a.b();
 }
 
-class A {
+abstract class A {
   void b();
 }
 ''', [
-      // TODO
+      lint(17, 24),
     ]);
   }
 
-  test_nullableIdentifierEqualEqualNull_null_elsePropertyAccess() async {
+  test_identifierEqualEqualNull_null_elsePrefixedIdentifier() async {
     await assertDiagnostics(r'''
 void f(A? a) {
-  a == null ? null : a.b.c;
-}
-
-class A {
-  A get b;
-  A get c;
-}
-''', [
-      // TODO
-    ]);
-  }
-
-  test_prefixed_equalEqualNull_null_elsePropertyAccess() async {
-    await assertDiagnostics(r'''
-void f(A? a) {
-  a.b == null ? null : a.b.c;
-''', [
-      // TODO
-    ]);
-  }
-
-  test_5() async {
-    await assertDiagnostics(r'''
-m(p) {
-  a.b == null ? null : a.b.c(); // LINT
-''', [
-      // TODO
-    ]);
-  }
-
-  test_6() async {
-    await assertDiagnostics(r'''
-  p == null ? null : p.b; // LINT
-m(p) {
-''', [
-      // TODO
-    ]);
-  }
-
-  test_7() async {
-    await assertDiagnostics(r'''
-m(p) {
-  null == a ? null : a.b; // LINT
-}
-''', [
-      // TODO
-    ]);
-  }
-
-  test_8() async {
-    await assertDiagnostics(r'''
-m(p) {
-  null == a ? null : a.b(); // LINT
-}
-''', [
-      // TODO
-    ]);
-  }
-
-  test_9() async {
-    await assertDiagnostics(r'''
-m(p) {
-  null == a.b ? null : a.b.c; // LINT
-}
-''', [
-      // TODO
-    ]);
-  }
-
-  test_nullEqualEqualIdentifier_null_elsePrefixedIdentifier() async {
-    await assertDiagnostics(r'''
-m(p) {
-  null == p ? null : p.b;
-}
-''', [
-      // TODO
-    ]);
-  }
-
-  test_identifierNotEqualPrefixedIdentifier_elseNull() async {
-    await assertDiagnostics(r'''
-void f(A? a) {
-  a != null ? a.b : null;
+  a == null ? null : a.b;
 }
 
 abstract class A {
   int get b;
 }
 ''', [
-      // TODO
+      lint(17, 22),
     ]);
   }
 
-  test_prefixedIdentifierNotEqualNull_propertyAccess_elseNull() async {
+  test_identifierEqualEqualNull_null_elsePropertyAccess() async {
     await assertDiagnostics(r'''
-void f(A a) {
-  a.b != null ? a.b!.c : null;
+void f(A? a) {
+  a == null ? null : a.b.c;
 }
 
 abstract class A {
-  A? get b;
+  A get b;
   int get c;
 }
 ''', [
-      // TODO
+      lint(17, 24),
     ]);
   }
 
-  test_nullableIdentifierNotEqualNull_prefixedIdentifier_null() async {
+  test_identifierEqualEqualNull_unrelatedBranch() async {
+    // This is covered by another rule.
+    await assertNoDiagnostics(r'''
+void f(int? a, int b) {
+  a == null ? b : a;
+}
+''');
+  }
+
+  test_identifierEqualEqualNull_unrelatedBranches() async {
+    await assertNoDiagnostics(r'''
+void f(int? a, int b) {
+  a == null ? b.isEven : null;
+}
+''');
+  }
+
+  test_identifierNotEqualNull_prefixedIdentifier_elseNull() async {
     await assertDiagnostics(r'''
 void f(A? a) {
   a != null ? a.b : null;
@@ -167,6 +100,63 @@ abstract class A {
 }
 ''', [
       lint(17, 22),
+    ]);
+  }
+
+  test_identifierNotEqualNull_prefixedIdentifier_null() async {
+    await assertDiagnostics(r'''
+void f(A? a) {
+  a != null ? a.b : null;
+}
+
+abstract class A {
+  int get b;
+}
+''', [
+      lint(17, 22),
+    ]);
+  }
+
+  test_nullEqualEqualIdentifier_null_elseMethodInvocation() async {
+    await assertDiagnostics(r'''
+void f(A? a) {
+  null == a ? null : a.b();
+}
+
+abstract class A {
+  void b();
+}
+''', [
+      lint(17, 24),
+    ]);
+  }
+
+  test_nullEqualEqualIdentifier_null_elsePrefixedIdentifier() async {
+    await assertDiagnostics(r'''
+void f(A? a) {
+  null == a ? null : a.b;
+}
+
+abstract class A {
+  int get b;
+}
+''', [
+      lint(17, 22),
+    ]);
+  }
+
+  test_nullEqualEqualPrefixedIdentifier_null_elsePropertyAccess() async {
+    await assertDiagnostics(r'''
+void f(A a) {
+  null == a.b ? null : a.b!.c;
+}
+
+abstract class A {
+  A? get b;
+  int get c;
+}
+''', [
+      lint(16, 27),
     ]);
   }
 
@@ -195,28 +185,41 @@ abstract class A {
   int get c;
 }
 ''', [
-      // TODO
+      lint(16, 27),
     ]);
   }
 
-  test_nullableIdentifierEqualEqualNull_unrelatedBranch() async {
-    // This is covered by another rule.
-    await assertNoDiagnostics(r'''
-void f(int? a, int b) {
-  a == null ? b : a;
+  test_prefixedIdentifierEqualEqualNull_null_elseMethodInvocation() async {
+    await assertDiagnostics(r'''
+void f(A a) {
+  a.b == null ? null : a.b!.c();
 }
-''');
+
+abstract class A {
+  A? get b;
+  void c();
+}
+''', [
+      lint(16, 29),
+    ]);
   }
 
-  test_nullableIdentifierEqualEqualNull_unrelatedBranches() async {
-    await assertNoDiagnostics(r'''
-void f(int? a, int b) {
-  a == null ? b.isEven : null;
+  test_prefixedIdentifierEqualEqualNull_null_elsePropertyAccess() async {
+    await assertDiagnostics(r'''
+void f(A a) {
+  a.b == null ? null : a.b!.c;
 }
-''');
+
+abstract class A {
+  A? get b;
+  int get c;
+}
+''', [
+      lint(16, 27),
+    ]);
   }
 
-  test_nullablePrefixedIdentifierNotEqualNull_prefixedIdentifier_elseNull() async {
+  test_prefixedIdentifierNotEqualNull_prefixedIdentifier_elseNull() async {
     await assertNoDiagnostics(r'''
 void f(A a) {
   a.b != null ? a.b : null;
@@ -228,15 +231,18 @@ abstract class A {
 ''');
   }
 
-  test_nullableIdentifierEqualEqualNull_null_elseBinaryExpression() async {
-    await assertNoDiagnostics(r'''
-void f(A? a) {
-  a == null ? null : a.b + 10;
+  test_prefixedIdentifierNotEqualNull_propertyAccess_elseNull() async {
+    await assertDiagnostics(r'''
+void f(A a) {
+  a.b != null ? a.b!.c : null;
 }
 
 abstract class A {
-  int get b;
+  A? get b;
+  int get c;
 }
-''');
+''', [
+      lint(16, 27),
+    ]);
   }
 }
